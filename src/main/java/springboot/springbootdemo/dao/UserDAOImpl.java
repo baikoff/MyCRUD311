84f0d23593528d32bc39c3ab438ea78e4e2e5ade
Artem Baykov <baikoff@bk.ru>
2022-06-19 15:20:23 +0300

    почти

---
+++ src/main/java/springboot/springbootdemo/dao/UserDAOImpl.java
@@ -0,0 +1,39 @@
+package springboot.springbootdemo.dao;
+
+import org.springframework.stereotype.Repository;
+import springboot.springbootdemo.model.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class UserDAOImpl implements UserDAO {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public List<User> getAllUsers() {
+        return entityManager.createQuery("SELECT c FROM users c", User.class).getResultList();
+    }
+
+
+    public void save(User user) {
+        entityManager.persist(user);
+    }
+
+
+    public User findById(int id) {
+        return entityManager.find(User.class, id);
+    }
+
+
+    public void update(User user, int id) {
+        entityManager.merge(user);
+    }
+
+
+    public void delete(int id) {
+        entityManager.remove(entityManager.find(User.class, id));
+    }
+}
---
+++ src/main/java/springboot/springbootdemo/service/UserService.java
@@ -0,0 +1,19 @@
+package springboot.springbootdemo.service;
+
+import springboot.springbootdemo.model.User;
+
+
+import java.util.List;
+
+public interface UserService {
+
+     void delete(int id);
+
+     void saveUser(User user);
+
+     User findById(int id);
+
+     List<User> getAllUsers();
+
+     void update(User user, int id);
+}
